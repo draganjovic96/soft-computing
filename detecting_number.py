61caf8f07dc82e89f84dedb806cb13858e474545
draganjovic96 <sasailickapiten22@gmail.com>
2019-07-07 23:32:28 +0200

    Added function for detecting blue and green line

--- detecting_number.py
+++ detecting_number.py
@@ -1,1 +1,13 @@
-from functions import video_to_frames
+from functions import detect_blue_and_green_line
+
+
+blue_and_green = detect_blue_and_green_line('video0-frame0.jpg')
+
+for x in blue_and_green:
+    print(x)
+
+
+
+
+
+
